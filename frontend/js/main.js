7c5206caff60b36c8ee2de63604cf6b1657a17c5
CichockiAdrian <adrian.cichocki@uczen.zsk.poznan.pl>
2025-05-06 08:21:32 +0200

    Added init.php

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,9 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Aplikacja zainicjalizowana!')
 
-  fetch('//api/get-posts.php')
-  .then(res => res.json())
-  .then(data => console.log(data));
+
+  fetch('/Silownia-Zdrowa-Igla/backend/api/init.php')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error("Fetch error:", error.message);
+  });
 
 
 })
