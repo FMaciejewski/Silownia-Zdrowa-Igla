e2c85a467d9d4e97b5e1b783881e736d1836943c
CichockiAdrian <adrian.cichocki@uczen.zsk.poznan.pl>
2025-05-06 07:44:34 +0200

    ~Vite delete

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,4 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Aplikacja zainicjalizowana!')
 
+  fetch('//api/get-posts.php')
+  .then(res => res.json())
+  .then(data => console.log(data));
+
+
 })
