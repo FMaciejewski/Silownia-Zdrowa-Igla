798dbbd0771feb558600944376046e658467cc20
CichockiAdrian <adrian.cichocki@uczen.zsk.poznan.pl>
2025-05-23 08:45:59 +0200

    Update profile.php

--- backend/api/profile.php
+++ backend/api/profile.php
@@ -1,9 +1,9 @@
 <?php
+header('Content-Type: application/json');
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 session_start();
-header('Content-Type: application/json');
 
 $db = new mysqli('localhost', 'root', '', 'silowniazdrowaigla');
 
